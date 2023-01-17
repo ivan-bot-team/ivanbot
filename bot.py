86411140e0f933b28ee3bb8f269e1743259350e9
Marti432 <martin1laszlo@gmail.com>
2023-01-17 21:18:17 +0100

    modals finished!

--- bot.py
+++ bot.py
@@ -28,6 +28,28 @@ intents.members = True
 
 # bot variable/object init
 bot = discord.Bot(intents=intents)
+#modals
+
+class MyModal(discord.ui.Modal):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.add_item(discord.ui.InputText(label="Názov písomky:"))
+        self.add_item(discord.ui.InputText(label="Dátum písomky:"))
+        self.add_item(discord.ui.InputText(label="Informácie k písomke:", style=discord.InputTextStyle.long))
+
+    async def callback(self, interaction: discord.Interaction):
+        embed = discord.Embed(title="Písomka")
+        embed.add_field(name="Názov písomky", value=self.children[0].value)
+        embed.add_field(name="Dátum písomky", value=self.children[1].value)
+        embed.add_field(name="Informácie", value=self.children[2].value)
+        await interaction.response.send_message(embeds=[embed])
+
+@bot.slash_command()
+async def create_exam(ctx: discord.ApplicationContext):
+    """Shows an example of a modal dialog being invoked from a slash command."""
+    modal = MyModal(title="Vytvoriť písomku")
+    await ctx.send_modal(modal)
 
 # launch
 @bot.event
