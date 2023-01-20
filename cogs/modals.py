e4f2cd70658ea902102209f1e0fd9c257699b1df
Ivan Polák <ivan.polak25@gmail.com>
2023-01-20 11:15:13 +0100

    train update and modal moved to cog

--- cogs/modals.py
+++ cogs/modals.py
@@ -3,24 +3,32 @@ import json
 import discord
 from discord.ext import tasks, commands
 
-#global_config = json.load(open('config.json', encoding='utf-8'))
-#config = global_config['modals']
 
-class MyModal(discord.ui.Modal):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+# global_config = json.load(open('config.json', encoding='utf-8'))
+# config = global_config['modals']
 
-        self.add_item(discord.ui.InputText(label="Short Input"))
-        self.add_item(discord.ui.InputText(label="Long Input", style=discord.InputTextStyle.long))
+
+class MyModal(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        # self.add_item(discord.ui.InputText(label="Názov písomky:"))
+        # self.add_item(discord.ui.InputText(label="Dátum písomky:"))
+        # self.add_item(discord.ui.InputText(label="Informácie k písomke:", style=discord.InputTextStyle.long))
 
     async def callback(self, interaction: discord.Interaction):
-        embed = discord.Embed(title="Modal Results")
-        embed.add_field(name="Short Input", value=self.children[0].value)
-        embed.add_field(name="Long Input", value=self.children[1].value)
+        embed = discord.Embed(title="Písomka")
+        embed.add_field(name="Názov písomky", value=self.children[0].value)
+        embed.add_field(name="Dátum písomky", value=self.children[1].value)
+        embed.add_field(name="Informácie", value=self.children[2].value)
         await interaction.response.send_message(embeds=[embed])
 
     @commands.slash_command()
-    async def modal_slash(ctx: discord.ApplicationContext):
-            """Shows an example of a modal dialog being invoked from a slash command."""
-            modal = MyModal(title="Modal via Slash Command")
-            await ctx.send_modal(modal)
+    async def create_exam(self, ctx):
+        """Shows an example of a modal dialog being invoked from a slash command."""
+        modal = MyModal(title="Vytvoriť písomku")
+        await ctx.send_modal(modal)
+
+
+def setup(bot):
+    bot.add_cog(Example(bot))
--- cogs/transit/trains.py
+++ cogs/transit/trains.py
@@ -1,15 +1,50 @@
-import random
 import json
 import slovakrailways as zsr
 from datetime import datetime, timedelta
 import discord
 from discord.ext import tasks, commands
 
-# helper.py
-from classes.helper import random_member
-
 global_config = json.load(open('config.json', encoding='utf-8'))
+
+
 # config = global_config['example']
+
+
+async def get_station_departures(station_name):
+    station = zsr.search_stations(station_name)[0]
+    departures = zsr.departures(station['uicCode'])[:10]
+    embed = discord.Embed(
+        title=f'Departures {station["name"]}',
+        # color=data['color'], TODO: fix this
+    )
+
+    train_types = zsr.meta.train_types()
+
+    for train in departures:
+        destination = train['station']
+        departure = str(datetime.fromtimestamp(float(str(train['timestamp'])[:-3])))[11:][:-3]
+        delay = train['train']['trainDelay']
+        if delay != None:
+            delay = f' + {delay["delayMinutes"]} min'
+        else:
+            delay = ''
+        train_type = next((type for type in train_types if type["value"] == train['train']['type']), {'name': 'KK'})[
+            'name']
+        train_number = train['train']['number']
+        embed.add_field(
+            name=destination,
+            value=str(
+                departure + delay + '\n**' + train_type + '** ' + str(train_number)
+            ),
+            inline=False,
+        )
+
+    embed.set_footer(text='Powered by Zajebane Slovenske Skurvene Kolaje')
+    embed.set_author(name='Felvidek')
+    # embed.set_thumbnail(url=data['thumbnail'])
+    # embed.set_image(url=data['image'])
+
+    return embed
 
 
 class Trains(commands.Cog):
@@ -18,27 +53,59 @@ class Trains(commands.Cog):
         self.bot = bot
 
     @commands.slash_command(
-            guild_ids=global_config['slash_commands_guilds'],
-            name='search-station',
-            description='search departures from a station'
+        guild_ids=global_config['slash_commands_guilds'],
+        name='search-station',
+        description='search departures from a station'
     )
     async def search_station(self, ctx, station: discord.Option(str, 'What station do you want to search?')):
+        try:
+            embed = await get_station_departures(station)
+            await ctx.respond(embed=embed)
+        except:
+            await ctx.respond('Zajebalo sa to, jebem tie zeleznice, skus ine meno stanice')
 
-        result = zsr.search_stations(station)
-        print(result)
-        departures = zsr.departures(result[0]['uicCode'])
-        departures = departures[:5]
-        # newdepartures = []
-        # for departure in departures:
-        #     departure['train']['features'] = ''
-        #     newdepartures.append(departure)
+    @commands.slash_command(
+        guild_ids=global_config['slash_commands_guilds'],
+        name='track-train',
+        description='track a train'
+    )
+    async def track_train(self, ctx, train_number: discord.Option(str, 'What train do you want to track(number)?')):
+        try:
+            train = zsr.track_train("273")
+            number = train['trainNumber']
+            # type = train['trainType']
+            previous_station = train['trainDelay']['previousStation']
+            current_station = train['trainDelay']['currentStation']
+            next_station = train['trainDelay']['nextStation']
+            delay = train['trainDelay']['delayMinutes']
 
-        departures[0]['train']['features'] = ''
-
-        await ctx.respond(departures[0])
-
-        # await ctx.respond(departures['station'])
-        # nz: 5613436
+            embed = discord.Embed(
+                title=f'Tracking train {number}',
+                # color=data['color'], TODO: fix this
+            )
+            embed.add_field(
+                name='Previous station',
+                value=previous_station,
+                inline=False,
+            )
+            embed.add_field(
+                name='Current station',
+                value=current_station,
+                inline=False,
+            )
+            embed.add_field(
+                name='Next station',
+                value=next_station,
+                inline=False,
+            )
+            embed.add_field(
+                name='Delay',
+                value=delay,
+                inline=False,
+            )
+            await ctx.respond(embed=embed)
+        except:
+            await ctx.respond('Jebem ich nejde im API')
 
     @commands.slash_command(
         guild_ids=global_config['slash_commands_guilds'],
@@ -46,26 +113,11 @@ class Trains(commands.Cog):
         description='search departures from nove zamky'
     )
     async def nove_zamky(self, ctx):
-        departures = zsr.departures('5613436')[:10]
-
-        embed = discord.Embed(
-            title='Departures Nové Zámky',
-            # color=data['color'], TODO: fix this
-        )
-
-        for train in departures:
-            embed.add_field(
-                name=train['station'],
-                value=str(datetime.fromtimestamp(int(train['timestamp'])).strftime('%Y-%m-%d %H:%M:%S')),
-                inline=False,
-            )
-
-        embed.set_footer(text='Powered by slovakrailways')
-        embed.set_author(name='Felvidek')
-        # embed.set_thumbnail(url=data['thumbnail'])
-        # embed.set_image(url=data['image'])
-
-        await ctx.respond(embed=embed)
+        try:
+            embed = await get_station_departures('Nové Zámky')
+            await ctx.respond(embed=embed)
+        except:
+            await ctx.respond('Zajebalo sa to, jebem tie zeleznice, skus ine meno stanice')
 
     @commands.Cog.listener()
     async def on_ready(self):
