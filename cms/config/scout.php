51c25fdde6d43419d78141613fb1a9976ab2d811
Ivan Polák <ivan.polak25@gmail.com>
2023-01-31 18:18:29 +0100

    expanded full text search

--- cms/config/scout.php
+++ cms/config/scout.php
@@ -8,7 +8,7 @@ return [
         'mode' => 'NATURAL_LANGUAGE',
         'model_directories' => [app_path()],
         'min_search_length' => 0,
-        'min_fulltext_search_length' => 2,
+        'min_fulltext_search_length' => 1,
         'min_fulltext_search_fallback' => 'LIKE',
         'query_expansion' => true
     ]
